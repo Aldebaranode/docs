8b8ea8046ea61a7cb124c627375172cd65d35f0c
mrheed <syahidnurrohim@gmail.com>
2025-05-10 23:56:13 +0700

    feat: add aztec sequencer guide

--- src/pages/guide/testnet/_meta.ts
+++ src/pages/guide/testnet/_meta.ts
@@ -1,8 +1,10 @@
 export default {
-  story: 'Story Protocol',
+  'story': 'Story Protocol',
+  'aztec-network': 'Aztec Network',
+  'eth-sepolia': 'ETH Sepolia',
   '0g': '0G Chain',
-  empeiria: 'Empeiria',
-  cysic: 'Cysic',
-  pipenetwork: 'Pipe Network',
-  cortensor: 'Cortensor'
+  'empeiria': 'Empeiria',
+  'cysic': 'Cysic',
+  'pipenetwork': 'Pipe Network',
+  'cortensor': 'Cortensor'
 }
---
+++ src/pages/guide/testnet/aztec-network/_meta.ts
@@ -0,0 +1,3 @@
+export default {
+  'sequencer-node': 'Sequencer Node'
+}
